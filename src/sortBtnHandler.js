4a7db18bbe1637e51579619aea24516fd5ca7ebf
YanaNeronskaya <yana.neronskaya@gmail.com>
2018-11-13 16:49:35 +0300

    init

--- src/sortBtnHandler.js
+++
@@ -1,7 +0,0 @@
-import fetchNewsChannel from "./fetchChannel";
-
-export const sortBtnHandler = (e) => {
-    const value = e.target.value;
-
-    fetchNewsChannel(localStorage.getItem('chosen_channel'), value)
-};
